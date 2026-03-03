00dfa9dd1c049ec1aedec7eb96c9042f51bdfe86
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-03 16:39:42 +0530

    Update Q4.java

--- ass-1/Q4.java
+++ ass-1/Q4.java
@@ -20,82 +20,81 @@ public class NHollow {
     }
 }
 
-----------DRY RUN for n=3-----------
-  n=3
-  i=1 i<=n (1<=3) -> True 
-    j=1 j<=n (1<=3) -> true 
-       if(i==1 || i==n || j==1 || j==n)
-       if(1==1 || 1==3 || 1==1 || 1==3)
-       if(true || false|| true || False) -> True
-           print *                                         o/p : *
-       j++ (j=2) 
-    j<=n (2<=3) -> True 
-        if(i==1 || i==n || j==1 || j==n)
-        if(1==1 || 1==3 || 2==1 || 2==3)
-        if(true || false|| false || False) -> True
-            print *                                          o/p : * *
-        j++ (j=3) 
-    j<=n (3<=3) -> True 
-        if(i==1 || i==n || j==1 || j==n)
-        if(1==1 || 1==3 || 3==1 || 3==3)
-        if(true || false|| false || true) -> True
-            print *                                            o/p : * * *
-        j++ (j=4)
-  j<=n (4<=3) -> False
---------------End inner loop-----------------   
-Next Line 
-i++ (i=2)
+// ----------DRY RUN for n=3-----------
+//   n=3
+//   i=1 i<=n (1<=3) -> True 
+//     j=1 j<=n (1<=3) -> true 
+//        if(i==1 || i==n || j==1 || j==n)
+//        if(1==1 || 1==3 || 1==1 || 1==3)
+//        if(true || false|| true || False) -> True
+//            print *                                         o/p : *
+//        j++ (j=2) 
+//     j<=n (2<=3) -> True 
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(1==1 || 1==3 || 2==1 || 2==3)
+//         if(true || false|| false || False) -> True
+//             print *                                          o/p : * *
+//         j++ (j=3) 
+//     j<=n (3<=3) -> True 
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(1==1 || 1==3 || 3==1 || 3==3)
+//         if(true || false|| false || true) -> True
+//             print *                                            o/p : * * *
+//         j++ (j=4)
+//   j<=n (4<=3) -> False
+// --------------End inner loop-----------------   
+// Next Line 
+// i++ (i=2)
 
-i=2 i<=n (2<=3) -> True 
-    j=1 j<=n (1<=3) -> true 
-       if(i==1 || i==n || j==1 || j==n)
-       if(2==1 || 2==3 || 1==1 || 1==3)
-       if(false || false|| true || False) -> True
-           print *                                           o/p : * * *
-       j++ (j=2)                                                   *
-    j<=n (2<=3) -> True 
-        if(i==1 || i==n || j==1 || j==n)
-        if(2==1 || 2==3 || 2==1 || 2==3)
-        if(false || false|| false || False) -> false
-            print _                                          o/p : * * *
-        j++ (j=3)                                                  * - 
-    j<=n (3<=3) -> True 
-        if(i==1 || i==n || j==1 || j==n)
-        if(1==1 || 1==3 || 3==1 || 3==3)
-        if(true || false|| false || true) -> True
-            print *                                          o/p : * * *
-        j++ (j=4)                                                  * - *
-  j<=n (4<=3) -> False
---------------End inner loop-----------------  
-Next Line 
-i++ (i=3)  
+// i=2 i<=n (2<=3) -> True 
+//     j=1 j<=n (1<=3) -> true 
+//        if(i==1 || i==n || j==1 || j==n)
+//        if(2==1 || 2==3 || 1==1 || 1==3)
+//        if(false || false|| true || False) -> True
+//            print *                                           o/p : * * *
+//        j++ (j=2)                                                   *
+//     j<=n (2<=3) -> True 
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(2==1 || 2==3 || 2==1 || 2==3)
+//         if(false || false|| false || False) -> false
+//             print _                                          o/p : * * *
+//         j++ (j=3)                                                  * - 
+//     j<=n (3<=3) -> True 
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(1==1 || 1==3 || 3==1 || 3==3)
+//         if(true || false|| false || true) -> True
+//             print *                                          o/p : * * *
+//         j++ (j=4)                                                  * - *
+//   j<=n (4<=3) -> False
+// --------------End inner loop-----------------  
+// Next Line 
+// i++ (i=3)  
 
-i=3 i<=n (3<=3) -> True 
-    j=1 j<=n (1<=3) -> true 
-       if(i==1 || i==n || j==1 || j==n)
-       if(3==1 || 3==3 || 1==1 || 1==3)
-       if(false || true|| true || False) -> True
-           print *                                           o/p : * * *
-       j++ (j=2)                                                   * - *
-    j<=n (2<=3) -> True                                            * 
-        if(i==1 || i==n || j==1 || j==n)
-        if(3==1 || 3==3 || 2==1 || 2==3)
-        if(false || true|| false || False) -> false
-            print *                                          o/p : * * *
-        j++ (j=3)                                                  * - * 
-    j<=n (3<=3) -> True                                            * *  
-        if(i==1 || i==n || j==1 || j==n)
-        if(3==1 || 3==3 || 3==1 || 3==3)
-        if(true || true|| false || true) -> True
-            print *                                          o/p : * * *
-        j++ (j=4)                                                  * - *
-  j<=n (4<=3) -> False                                             * * * 
---------------End inner loop----------------- 
-  next Line 
-  i++ (i=4) i<=n (4<=3) -> False
-  -----------end outer loop-------------------
+// i=3 i<=n (3<=3) -> True 
+//     j=1 j<=n (1<=3) -> true 
+//        if(i==1 || i==n || j==1 || j==n)
+//        if(3==1 || 3==3 || 1==1 || 1==3)
+//        if(false || true|| true || False) -> True
+//            print *                                           o/p : * * *
+//        j++ (j=2)                                                   * - *
+//     j<=n (2<=3) -> True                                            * 
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(3==1 || 3==3 || 2==1 || 2==3)
+//         if(false || true|| false || False) -> false
+//             print *                                          o/p : * * *
+//         j++ (j=3)                                                  * - * 
+//     j<=n (3<=3) -> True                                            * *  
+//         if(i==1 || i==n || j==1 || j==n)
+//         if(3==1 || 3==3 || 3==1 || 3==3)
+//         if(true || true|| false || true) -> True
+//             print *                                          o/p : * * *
+//         j++ (j=4)                                                  * - *
+//   j<=n (4<=3) -> False                                             * * * 
+// --------------End inner loop----------------- 
+//   next Line 
+//   i++ (i=4) i<=n (4<=3) -> False
+//   -----------end outer loop-------------------
 
-  Time compexity : O(n^2)
-  Space Compexity : O(1)
+
   
   
