8e274a0e80ea6fedfd306e0851fccc0fa52cd3d5
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-03 19:27:03 +0530

    Update Q3.java

--- ass-2/Q3.java
+++ ass-2/Q3.java
@@ -14,47 +14,45 @@ public class RightTriangle {
     
 }
 
-------------------DRY RUN for 3--------------------
-  n=3
-  i=1 i<=n (1<=3) -> true
-    j=1 j<=i (1<=1) -> true
-      print *                     o/p : *
-      j++ (j=2)
-    j=2 j<=i (2<=1) -> false
-  --------------end inner loop ---------------------
-  next line 
-  i++ (i=2)
-  i=2 i<=n (2<=3) -> true
-    j=1 j<=i (1<=2) -> true
-      print *                     o/p : *
-      j++ (j=2)                         * 
-    j=2 j<=i (2<=2) -> true
-        print *                     o/p : *
-      j++ (j=3)                           * * 
-    j=3 j<=i (3<=2) -> false
-  --------------end inner loop ---------------------
-  next line 
-  i++ (i=3)
-    j=1 j<=i (1<=3) -> true
-        print *                     o/p : *
-      j++ (j=2)                           * * 
-                                          * 
+// ------------------DRY RUN for 3--------------------
+//   n=3
+//   i=1 i<=n (1<=3) -> true
+//     j=1 j<=i (1<=1) -> true
+//       print *                     o/p : *
+//       j++ (j=2)
+//     j=2 j<=i (2<=1) -> false
+//   --------------end inner loop ---------------------
+//   next line 
+//   i++ (i=2)
+//   i=2 i<=n (2<=3) -> true
+//     j=1 j<=i (1<=2) -> true
+//       print *                     o/p : *
+//       j++ (j=2)                         * 
+//     j=2 j<=i (2<=2) -> true
+//         print *                     o/p : *
+//       j++ (j=3)                           * * 
+//     j=3 j<=i (3<=2) -> false
+//   --------------end inner loop ---------------------
+//   next line 
+//   i++ (i=3)
+//     j=1 j<=i (1<=3) -> true
+//         print *                     o/p : *
+//       j++ (j=2)                           * * 
+//                                           * 
   
-    j=2 j<=i (2<=3) -> true               
-       print *                      o/p : *
-       j++ (j=3)                          * * 
-                                          * *  
-    j=3 j<=i (3<=3) -> true        
-       print *                       o/p : *
-       j++ (j=4)                           * *
-                                           * * * 
-  j=4 j<=i (4<=3)  -> false
-----------------------end inner loop-------------------
-  next line 
-  i++ (i=4)
-  i<=n (4<=3) -> false
-  -------------------- end outer loop -----------------
+//     j=2 j<=i (2<=3) -> true               
+//        print *                      o/p : *
+//        j++ (j=3)                          * * 
+//                                           * *  
+//     j=3 j<=i (3<=3) -> true        
+//        print *                       o/p : *
+//        j++ (j=4)                           * *
+//                                            * * * 
+//   j=4 j<=i (4<=3)  -> false
+// ----------------------end inner loop-------------------
+//   next line 
+//   i++ (i=4)
+//   i<=n (4<=3) -> false
+//   -------------------- end outer loop -----------------
   
-  time Complexity : O(n^2)
-  space Complexity : O(1)
-  
+ 
