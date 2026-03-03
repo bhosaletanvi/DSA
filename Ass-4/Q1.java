f3003d173ee1b723ff8d6efaf8b3ca0a192bf5ee
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-03 10:13:44 +0530

    Update Q1.java

--- Ass-4/Q1.java
+++ Ass-4/Q1.java
@@ -15,15 +15,61 @@ public static void main(String[] args){
 }
 }
 
+
+// ----------DRY run for 3 -----------
 // n=3
-i=1 i<=n (1<=3) -> True 
-  j=1 j<=n (1<=3) -> true
-    print *_
-    j++ -> (j=2)
-      j<=n (2<=3) -> true 
-      print *_
-      j++ ->(j=3)
-        j<=n (3=3) -> true
-        print *_  
-      
+// i=1 i<=n (1<=3) -> True 
+//   j=1 j<=n (1<=3) -> true
+//     print *_
+//     j++ -> (j=2)
+//       j<=n (2<=3) -> true 
+//       print *_
+//       j++ ->(j=3)
+//         j<=n (3=3) -> true
+//         print *_  
+//         j++ -> (j=4)
+//         j<=n (4<=3) -> False
+//  --------End inner Loop----------       
+// next Line 
+// i++ (i=2) 
+//  i=2 i<=n (2<=3) -> True 
+//   j=1 j<=n (1<=3) -> true
+//     print *_
+//     j++ -> (j=2)
+//       j<=n (2<=3) -> true 
+//       print *_
+//       j++ ->(j=3)
+//         j<=n (3=3) -> true
+//         print *_  
+//         j++ -> (j=4)
+//         j<=n (4<=3) -> False
+//  --------End inner Loop----------       
+// next Line 
+// i++ (i=3)
+// i=3 i<=n (3<=3) -> True 
+//   j=1 j<=n (1<=3) -> true
+//     print *_
+//     j++ -> (j=2)
+//       j<=n (2<=3) -> true 
+//       print *_
+//       j++ ->(j=3)
+//         j<=n (3=3) -> true
+//         print *_  
+//         j++ -> (j=4)
+//         j<=n (4<=3) -> False
+//  --------End inner Loop----------       
+// next Line     
+// i++ (i=4)
+// i=4 i<=n (4<=3) -> False     
+// --------outer loop end-----------   
+
+//     Output : 
+//     * * *
+//     * * * 
+//     * * *
+
+//  Time Compexity : O(n^2)
+//  space Compexity : O(1)   
+    
+    
       
