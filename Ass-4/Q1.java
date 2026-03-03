ba7c285b99ef2bda2e8446f32e4d99839ed4eb3a
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-03 10:07:13 +0530

    Update Q1.java

--- Ass-4/Q1.java
+++ Ass-4/Q1.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+public class Nbyn{
+public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter Number : ");
 
+    int n =s.nextInt();
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            System.err.print("* ");
+        }
+        System.out.println();
+    }
+}
+}
+
+// n=3
+i=1 i<=n (1<=3) -> True 
+  j=1 j<=n (1<=3) -> true
+    print *_
+    j++ -> (j=2)
+      j<=n (2<=3) -> true 
+      print *_
+      j++ ->(j=3)
+        j<=n (3=3) -> true
+        print *_  
+      
+      
