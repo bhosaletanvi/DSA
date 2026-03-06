9c7591d0f663a04186d116150c95b833585cc969
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-06 19:20:57 +0530

    Update Q4.java

--- ass-5/Q4.java
+++ ass-5/Q4.java
@@ -18,3 +18,40 @@ public class Pelindrome{
        }
     }
 }
+
+// --------------------- DRY RUN ------------------------------
+//   n=121
+// original =n -> 121
+// reverse =0
+//   while (n!=0) (121 !=0) -> true
+//     digit = n%10 (121 %10 ) ->  1
+//     reverse=reverse*10+digit (0*10 +1 ) -> 1
+//     n=n/10 (121/10) ->12
+
+//  while (n!=0) (12 !=0) -> true
+//     digit = n%10 (12 %10 ) ->  2
+//     reverse=reverse*10+digit (1*10 +2 ) -> 12
+//     n=n/10 (12/10) ->1
+
+//  while (n!=0) (1 !=0) -> true
+//     digit = n%10 (1 %10 ) ->  1
+//     reverse=reverse*10+digit (12*10 +1 ) -> 121
+//     n=n/10 (1/10) ->0
+
+//  while (n!=0) (0!=0) -> False 
+
+// if(oroginal== reverse)
+//     121 == 121 -> true
+//         print Pelindrome Number
+// --------------- End while loop -------------------------------
+  
+
+//   o/p :
+
+// Enter Number : 121
+// Pelindrome Number
+
+// Enter Number : 321
+// Not Pelindrome Number
+  
+  
