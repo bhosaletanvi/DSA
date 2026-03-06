b69c43f9bed6bca163580a1e93b0589f786cf8de
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-03-06 20:01:17 +0530

    Update Q6.java

--- ass-5/Q6.java
+++ ass-5/Q6.java
@@ -9,7 +9,6 @@ public class AmstrongNo {
        int original=n; 
        int n2=n;
        while(n!=0){
-        int digit=n%10;
         n=n/10;
         count++;
        }
@@ -30,3 +29,55 @@ public class AmstrongNo {
 }
     
 
+// --------------------- DRY RUN ------------------------------
+//   count =0 
+//   n=153
+// original= n -> 153
+// n2 =n -> 153
+//   while (n!=0) (153 !=0) -> true
+//     n=n/10 (153/10) ->15
+//     count ++ (0+1) -> 1
+
+//  while (n!=0) (32 !=0) -> true
+//     n=n/10 (15/10) ->1
+//     count ++ (1+1) -> 2  
+
+//  while (n!=0) (3 !=0) -> true
+//     n=n/10 (1/10) ->0
+//     count ++ (2+1) -> 3
+
+//  while (n!=0) (0!=0) -> False 
+  
+// --------------- End while loop -------------------------------
+  
+        // sum=0
+//   while (n2!=0) (153 !=0) -> true
+//         digit=n2%10 -> (153 % 10) -> 3
+//         n2=n2/10 -> (153/10) -> 15
+//         sum=sum+ Math.pow(digit,count) -> (0+27) -> 27        
+
+//   while (n2!=0) (15 !=0) -> true
+//         digit=n2%10 -> (15 % 10) -> 5
+//         n2=n2/10 -> (15/10) -> 1
+//         sum=sum+ Math.pow(digit,count) -> (27+125) -> 152  
+
+//   while (n2!=0) (15 !=0) -> true
+//         digit=n2%10 -> (1 % 10) -> 1
+//         n2=n2/10 -> (1/10) -> 0
+//         sum=sum+ Math.pow(digit,count) -> (152+1) -> 153
+
+//  while (n2!=0) (0!=0) -> False 
+
+// -------------------End 2ns while loop ------------------------
+   // if (oroginal == sum)
+   //    (153==153) -> true
+   //         print  amstrong Number 
+//   o/p :
+
+// Enter Number : 153
+// Amstrong Number
+
+// Enter Number : 162
+// not Amstrong Number
+  
+  
