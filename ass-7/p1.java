ce87dd2c16a36f8ff823b8c073be8752b9be42b2
Tanvi <bhosaletanvi59@gmail.com>
2026-03-28 23:01:39 +0530

    file updated

--- ass-7/p1.java
+++ ass-7/p1.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class p1{
     public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int [] arr = new int [size];
+        System.out.print("Enter Elemets : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
         
     }
 }
