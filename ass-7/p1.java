f0bc346fb4af7d5bc57616220474b1f352cf4f75
Tanvi <bhosaletanvi59@gmail.com>
2026-03-28 23:08:59 +0530

    file updated

--- ass-7/p1.java
+++ ass-7/p1.java
@@ -9,6 +9,20 @@ class p1{
         for (int i = 0; i < arr.length; i++) {
             arr[i]=sc.nextInt();
         }
-        
+        System.out.print("Enter Target  : ");
+        int target = sc.nextInt();
+        int ans = f(arr,target,0);
+        System.out.println("first Occurance at : "+ans);
+    }
+        static int f(int [] arr , int target , int index){
+            if(arr[index]==target){
+                return index;
+            }
+            if(index == arr.length-1){
+                System.out.println("Element not found ");
+                return -1;
+            }
+            return   f(arr,target,index+1);
+
     }
 }
