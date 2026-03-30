9db2cc63c5552fa2ccfc4587915f3516fbdcdb8d
Tanvi <bhosaletanvi59@gmail.com>
2026-03-30 22:53:07 +0530

    files  updated

---
+++ ass-7/Greater.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Greater {
+    public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int [] arr = new int [size];
+        System.out.print("Enter Elemets : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Enter X value : ");
+        int x = sc.nextInt();
+        int ans = f(arr, 0, x);
+        System.out.println(ans);
+    }
+
+    static int count = 0;
+
+    static int f(int[] arr, int index, int target) {
+
+        if (index == arr.length - 1) {
+            if (arr[index] > target) {
+                return 1;
+            }
+            return 0;
+        }
+
+        int right = f(arr, index + 1, target);
+
+        if (arr[index] > target) {
+            ++count;
+        }
+
+        return count;
+    }
+}
---
+++ ass-7/target.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class target{
+    public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int [] arr = new int [size];
+        System.out.print("Enter Elemets : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+         System.out.print("Enter target : ");
+        int target = sc.nextInt();
+        boolean ans=f(arr,0,target);
+        System.out.println(ans);
+    }   
+    static boolean   f(int [] arr, int index,int target){
+      if(index==arr.length-1){
+        if(arr[index]==target){
+            return true;
+        }
+        return false;
+      }
+      if(arr[index]==target){
+            return true;
+        }
+        return f(arr, index+1, target);
+    }
+}
+
