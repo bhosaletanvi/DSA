888fab29ef56c1f3f8ef989b77d20b83aff0f5ad
Tanvi <bhosaletanvi59@gmail.com>
2026-03-30 19:32:49 +0530

    ass-4 files  name changed

--- Ass-4/FourVnum.java
+++ Ass-4/FourVnum.java
@@ -1,11 +1,11 @@
-public class FourHnum{
+public class FourVnum{
     public static void main(String[] args) {
         int i,j;
         for(i=1;i<=4;i++){
             for(j=1;j<=4;j++){
                 System.out.print(" "+j);
             }
-            System.err.println();
+            System.out.println();
         }
     }
 }
--- Ass-4/NCross.java
+++ Ass-4/NCross.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class NCross {
-    void main(){
+    public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter Number : ");
         int n=sc.nextInt();
