94c5bb11f4b342fdd34bb76ac6d3728a0dc30c29
Tanvi <bhosaletanvi59@gmail.com>
2026-04-01 00:16:33 +0530

    file name changed

--- ass-2/p1.java
+++ ass-2/p1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class NHollow {
+ class NHollow {
      public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number : ");
--- ass-2/p2.java
+++ ass-2/p2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class NCross {
+ class NCross {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter Number : ");
