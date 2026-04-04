11898ac7af84f9d69ca88c704a3f20da330f0691
Tanvi <bhosaletanvi59@gmail.com>
2026-04-04 21:27:31 +0530

    file added

---
+++ ass-7/using_for_loop/p13.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Grated_than_X{
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.print("Enter array Size :");
+    int size=sc.nextInt();
+    int[] arr=new int[size];
+    System.out.print("Enter array elements : ");
+    for (int i = 0; i <arr.length; i++) {
+        arr[i]=sc.nextInt();
+    } 
+    System.out.print("Enter Value of K:");
+    int k=sc.nextInt();  
+    int count=0;
+    for (int i = 0; i < arr.length; i++) {
+        if(arr[i]>k){
+            count++;
+        }    
+    }
+    System.out.print("array have "+count+" elements greater than "+k);
+    }
+    
+}
