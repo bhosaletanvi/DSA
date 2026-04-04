c28840daae998a842e0953f1a9f261ec48cd3121
Tanvi <bhosaletanvi59@gmail.com>
2026-04-04 22:04:46 +0530

    file added

---
+++ ass-7/using_for_loop/p20.java
@@ -0,0 +1,50 @@
+import java.util.*;
+ class Stricktly_decresing {
+    public static void main(String[] args) {
+         Scanner sc =new Scanner(System.in);
+        System.out.print("Enter Array Size : ");
+        int size = sc.nextInt();
+        int [] arr = new int [size];
+        System.out.print("Enter Elemets : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+            int max=0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+        }
+
+        int[] freq = new int[max + 1];
+        for (int j = 0; j < arr.length; j++) {
+            freq[arr[j]]++;
+        }
+
+        boolean flag = false;
+
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] <= 1) {
+                flag = true;
+            } else {
+                flag = false;
+                System.out.print(flag);
+                break;
+            }
+        }
+        boolean ans=false;
+        if (flag) {
+            for (int i = 1; i < arr.length; i++) {
+                if(arr[i-1]>arr[i]){
+                    ans=true;
+                }
+                else{
+                    ans=false;
+                    break;
+                }
+            }
+            System.out.println(ans);
+            
+        } 
+    }
+
+   
+}
