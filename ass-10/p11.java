045f306b53cead1059017ef3c9e80ae0117adf2f
Tanvi <bhosaletanvi59@gmail.com>
2026-05-05 00:39:27 +0530

    file added

--- ass-10/p11.java
+++ ass-10/p11.java
@@ -5,26 +5,14 @@ class demo{
 		System.out.print("Enter Array Size : ");
 		int size = sc.nextInt();
 		int [] arr = new int[size];
-		
 		System.out.print("Enter array element : ");
 		for(int i = 0; i<arr.length;i++){
 			arr[i]=sc.nextInt();
 		}
 		int max =0;
-		for(int i = 0; i<arr.length;i++){
-			max=Math.max(max,arr[i]);
+		for(int i =0;i<arr.length;i++)	{
+			max=Math.max()
 		}
-	
-		int [] freq=new int [max+1];
-		for(int i = 0; i<arr.length;i++){
-			freq[arr[i]]++;
-		}
-		for(int i = 0; i<freq.length;i++){
-			if(freq[i]!=0){
-				System.out.println(i+" : "+freq[i]);
-			}
-		}
-		
+		System.out.print("larger number : ",max);	
 	}
-	
 }
