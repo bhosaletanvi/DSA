469b7ebfbfb3f7adb6e25faca6e317e97f0c23d1
Tanvi <bhosaletanvi59@gmail.com>
2026-06-09 14:26:00 +0530

    file added

--- ass-12/p4.java
+++ ass-12/p4.java
@@ -2,14 +2,21 @@ import java.util.*;
 class demo{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter String : ");
-		String s= sc.nextLine();
+		
+		System.out.println("Enter Size : ");
+		int size= sc.nextInt();
+		String [] sentence = new String[size];
+		for(int i=0;i<sentence.length;i++){
+		System.out.println("Enter sentence "+(i+1)+" : ");
+
+			sentence[i]=sc.nextLine();
+			sc.nextLine();
+		}
 		int max=0;
-		String [] sentence = s.split(" ");
 		for(int i=0;i<sentence.length;i++){
 			String temp = sentence[i];
 			String [] str = temp.split(" ");
-			int count=str.length-1;
+			int count=str.length;
 			max=Math.max(count,max);
 		}			
 		System.out.println(max);
