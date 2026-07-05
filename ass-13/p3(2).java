605e9925f0193f283be4a4c16313776d81bdd337
tanvi bhosale <bhosaletanvi59@gmail.com>
2026-07-06 00:43:44 +0530

    file added

--- ass-13/p3(2).java
+++ ass-13/p3(2).java
@@ -14,47 +14,19 @@ class demo{
 		System.out.println(totalTrip(arr,tp));
     	}
 	static int totalTrip(int [] arr, int tp){
-		int mincount=Integer.MAX_VALUE,count=0,temp=0,time=0,ans=0;
-		boolean flag=true;
-		for(int j=0;j<arr.length;j++){
-			System.out.println(arr[j]+"-----------");
-			time=arr[j];
-			count=0;
-			flag=true;
-			//index=0;	
-			for(int i=0;i<arr.length;i++){
-				if(index==arr.length-1){
-					break;
-				}else if (i==arr.length){
-					//temp=arr[index];
-					//count=0;	
-					
-				}
-				temp=time;
-				System.out.println(arr[i]+": var");
-				if(arr[i]<=time){
-					while(temp>0){
-						count++;
-						temp=temp-arr[i];
-						System.out.println(temp+": temp updated"+count);
-					if(temp<arr[i]){
-						break;
-					}
-
-					}
-				}else{
-					flag=false;
-					break;
-				}
-			}
-			if(flag && count>=tp){
-				mincount=Math.min(mincount,count);
-				System.out.println(mincount);
-				ans=time;
-			}
-			//index++;
+		int time=0,count=0;
+		while(count<tp){
 			
+			for(int i=0;i<arr.length;i++){
+				if(arr[i]<=time){
+					int digit=time/arr[i];
+					count+=digit;
+				}else{
+					break;
+				}
+			}
+			time++;
 		}
-		return ans;
+		return time-1;		
 	}
 }
--- ass-13/p3.java
+++ ass-13/p3.java
@@ -14,32 +14,25 @@ class demo{
 		System.out.println(totalTrip(arr,tp));
     	}
 	static int totalTrip(int [] arr, int tp){
-		int mincount=Integer.MAX_VALUE,count=0,temp=0,time=1,ans=0;
+		 long mincount=Integer.MAX_VALUE,count=0,temp=0,time=1,ans=0;
 		while(count<tp){
 			count=0;
-			flag=true;
 			for(int i=0;i<arr.length;i++){
-				temp=arr[i];
+				temp=time;
 				if(arr[i]<=time){
 					while(temp>0){
 						count++;
 						temp-=arr[i];
-						if(arr[i]-temp<0){
+						if(temp-arr[i]<0){
 							break;	
 						}
 					}
-				}else{
-					flag=false;
-					break;
 				}
-			}
-			if(flag && count>=tp){
-				mincount=Math.min(mincount,count);
-				ans=time;
-				
 			}
 			time++;
 
-		} 	
+		} 
+		return time-1;
+	
 	}
 }
